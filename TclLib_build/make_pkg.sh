0ad166a384dc9c2284ab81e167b1f85b57b60540
Marc Culler <culler@users.noreply.github.com>
2025-10-11 21:57:00 -0500

    Add TclLib build directory

---
+++ TclLib_build/make_pkg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ../IDs.sh
+TCLLIB_DIR=`ls dist/lib | grep 'tcllib\([0-9]*\.[0=9]*\)'`
+TCLLIB_VER=`echo ${TCLLIB_DIR} | sed 's/tcllib//'`
+TCL_VER=`readlink ../TclTk_build/build/tk/Tcl.framework/Versions/Current`
+FRAMEWORK="/Library/Frameworks/Tcl.framework"
+INSTALL_DIR=${FRAMEWORK}/Versions/${TCL_VER}/Resources/Scripts
+mkdir -p temp
+pkgbuild --root dist/lib/${TCLLIB} \
+         --identifier core.tcl-lang.org-tcllib${TCLLIB_VER} \
+         --version ${TCLLIB_VER} \
+         --install-location ${INSTALL_DIR} \
+         temp/tcllib.pkg ;
+productsign --sign $DEV_ID temp/tcllib.pkg tcllib.pkg
